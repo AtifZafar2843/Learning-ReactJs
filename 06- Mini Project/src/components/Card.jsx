3ba171d47b518f011734277eefab05ce5db81166
Atif Zafar <tgatifzafar@gmail.com>
2025-11-03 14:27:08 +0530

    add Mini project folder

---
+++ 06- Mini Project/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+    <div className="card">
+    <div className="upper">
+        <div className="logo"></div>
+        <div className="sv-btn"></div>
+    </div>
+    <div className="center"></div>
+    <div className="bottom"></div>
+    </div>
+    </>
+  )
+}
+
+export default Card
