6a16be188861562b0a6a3c65193888628ab0cfa6
Atif Zafar <tgatifzafar@gmail.com>
2025-11-05 12:11:06 +0530

    make card design

--- 06- Mini Project/src/components/Card.jsx
+++ 06- Mini Project/src/components/Card.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
+import 'remixicon/fonts/remixicon.css'
 
 const Card = () => {
   return (
     <>
     <div className="card">
     <div className="upper">
-        <div className="logo"></div>
-        <div className="sv-btn"></div>
+        <div className="logo">
+          <img src="https://cdn.freebiesupply.com/images/large/2x/airbnb-logo.png" alt="airbnb logo" />
+        </div>
+        <div className="sv-btn">
+          <button>Save</button>
+          <i className="ri-bookmark-line"></i>
+        </div>
     </div>
-    <div className="center"></div>
-    <div className="bottom"></div>
+    <div className="center">
+      <div className="center-top">
+      <h3>Airbnb</h3><span>5 days ago</span>
+      </div>
+      <h2>Junior UI/UX Designer</h2>
+      <div className="tag">
+        <h4>Contract</h4>
+        <h4>Remote</h4>
+      </div>
+    </div>
+    <div className="bottom">
+      <div className="bottom-left">
+        <h5>$100/hr</h5>
+        <h6>Delhi, India</h6>
+      </div>
+      <div className="bottom-right">
+        <button>Apply Now</button>
+      </div>
+    </div>
     </div>
     </>
   )
