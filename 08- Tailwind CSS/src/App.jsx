d85740a8cfb332b318696b95075765b997a23f8f
Atif Zafar <tgatifzafar@gmail.com>
2025-11-07 12:44:32 +0530

    install tailwind css

---
+++ 08- Tailwind CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen w-full bg-gray-500'>
+          <h1 className='text-white'>Atif Zafar</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
