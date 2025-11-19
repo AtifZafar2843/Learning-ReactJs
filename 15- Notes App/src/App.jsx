1ba5ea26b0824a8027e5d5d8ed7e44c5e756d4c3
Atif Zafar <tgatifzafar@gmail.com>
2025-11-19 15:31:49 +0530

    make basic ui of app

--- 15- Notes App/src/App.jsx
+++ 15- Notes App/src/App.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div
-      className=''
-    >
-      <div
-        className='flex flex-col bg-red-500'>
+    <div className="bg-gray-600 h-screen flex gap-18 items-center px-40">
+      <div className="flex flex-col gap-4 text-white w-full">
         <input
           type="text"
-          placeholder='Enter your task.'
-          className=''
+          placeholder="Enter your task."
+          className="border-2 w-full py-2 px-4 rounded-lg outline-none"
         />
         <textarea
-          placeholder='Enter details.'
+          placeholder="Enter details."
           name=""
           id=""
-          className=''
+          className="border-2 h-30 w-full py-2 px-4 rounded-lg outline-none"
         ></textarea>
-        <button
-          className=''
-        >Add Task</button>
+        <button className=" w-full py-2 px-4 rounded-lg outline-none bg-white text-black">
+          Add Task
+        </button>
       </div>
+      <img
+        className='h-50'
+        src="https://cdn-icons-png.flaticon.com/512/4021/4021693.png" />
     </div>
-  )
+  );
 }
 
 export default App
