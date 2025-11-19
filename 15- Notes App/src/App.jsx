d6e5f20c4785776ad8d18f11f991507d572a8627
Atif Zafar <tgatifzafar@gmail.com>
2025-11-19 15:10:48 +0530

    start making notes app

---
+++ 15- Notes App/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div
+      className=''
+    >
+      <div
+        className='flex flex-col bg-red-500'>
+        <input
+          type="text"
+          placeholder='Enter your task.'
+          className=''
+        />
+        <textarea
+          placeholder='Enter details.'
+          name=""
+          id=""
+          className=''
+        ></textarea>
+        <button
+          className=''
+        >Add Task</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
