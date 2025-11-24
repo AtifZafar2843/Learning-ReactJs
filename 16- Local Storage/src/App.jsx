cc57f1dc764fb2df638598f0ec34f0d521ffd2d7
Atif Zafar <tgatifzafar@gmail.com>
2025-11-24 12:50:17 +0530

    learn about JSON parse and srtingyfy

--- 16- Local Storage/src/App.jsx
+++ 16- Local Storage/src/App.jsx
@@ -15,6 +15,21 @@ const App = () => {
 
   // localStorage.removeItem('user') ye item remove karne ke kaam aata hai
 
+
+  const customerDetail = {
+    name: "Atif",
+    age: 18,
+    city: "Aligarh"
+  }
+
+  // yaha hum dekh rahe hain kaise kisi object ya array ko local storage me save karte hain.
+  localStorage.setItem("customer", JSON.stringify(customerDetail));
+
+  // ab yaha hum dekhenge k kisi object ya array ko local storage kaise get karenge
+
+  const customer = JSON.parse(localStorage.getItem("customer"))
+  // kyu ki ye data string ke form me tha to is liye isko dubar parse use karke object form me convert kara hai
+  console.log(customer);
   
   
   localStorage.getItem()
