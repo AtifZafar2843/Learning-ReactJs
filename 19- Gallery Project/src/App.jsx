eadba2e1cbc6120d020b7d55cdbf1eebf6e76513
Atif Zafar <tgatifzafar@gmail.com>
2025-12-08 17:44:44 +0530

    gallery project completed

--- 19- Gallery Project/src/App.jsx
+++ 19- Gallery Project/src/App.jsx
@@ -1,37 +1,76 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
   const [userdata, setUserData] = useState([]);
+  const [index, setIndex] = useState(1);
+
   const getData = async () => {
-    const response = await axios.get("https://picsum.photos/v2/list");
+    const response = await axios.get(
+      `https://picsum.photos/v2/list?page=${index}&limit=10`
+    );
     setUserData(response.data);
   };
 
-  let printUserData = "No user Available";
+  useEffect(
+    function () {
+      getData();
+    },
+    [index]
+  );
+
+  let printUserData = (
+    <h1 className="text-gray-600 font-bold text-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      Loading...
+    </h1>
+  );
   if (userdata.length > 0) {
     printUserData = userdata.map(function (elem, idx) {
       return (
-        <div>
-          <div className="h-50 w-50 overflow-hidden bg-red-400 rounded-2xl">
-            <img className="h-full w-full object-cover" src={elem.download_url} />
-          </div>
-          <h1
-            className="font-bold"
-          >{elem.author}</h1>
+        <div key={idx}>
+          <a href={elem.url} target="_blank">
+            <div className="h-50 w-50 overflow-hidden bg-red-400 rounded-2xl">
+              <img
+                className="h-full w-full object-cover"
+                src={elem.download_url}
+              />
+            </div>
+            <h1 className="font-bold text-white">{elem.author}</h1>
+          </a>
         </div>
       );
     });
   }
   return (
-    <div className="p-4 overflow-auto">
-      <button
-        onClick={getData}
-        className="bg-blue-800 active:scale-95 text-white px-8 py-2 rounded"
-      >
-        Get Data
-      </button>
-      <div className="mt-4 flex flex-wrap gap-4 items-center justify-center">{printUserData}</div>
+    <div className="p-4 overflow-auto h-screen bg-black">
+      <div className="flex h-[90%] flex-wrap gap-4 items-center justify-center">
+        {printUserData}
+      </div>
+      <div className="flex items-center justify-center gap-6">
+        <button
+          onClick={() => {
+            if (index > 1) {
+              setIndex(index - 1);
+            }
+            setUserData([]);
+          }}
+          className="py-2 px-4 bg-amber-400 text-black active:scale-95 rounded font-semibold text-sm"
+        >
+          Prev
+        </button>
+        <h2 className="text-white">page {index}</h2>
+        <button
+          onClick={() => {
+            if (index < 10) {
+              setIndex(index + 1);
+            }
+            setUserData([]);
+          }}
+          className="py-2 px-4 bg-amber-400 text-black active:scale-95 rounded font-semibold text-sm"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
