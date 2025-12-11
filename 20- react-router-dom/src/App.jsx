f8c1953e1666207d67659f31da5fde40a5efdaea
Atif Zafar <tgatifzafar@gmail.com>
2025-12-11 14:51:38 +0530

    Add routes

--- 20- react-router-dom/src/App.jsx
+++ 20- react-router-dom/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
   )
 }
 
