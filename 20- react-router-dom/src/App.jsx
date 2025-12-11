1ddcc5d9bb9239b8abedee6fd1279dfe894f2010
Atif Zafar <tgatifzafar@gmail.com>
2025-12-11 15:29:55 +0530

    add no realod link

--- 20- react-router-dom/src/App.jsx
+++ 20- react-router-dom/src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
